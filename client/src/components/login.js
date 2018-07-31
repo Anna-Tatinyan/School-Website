34b0861cd7ce67cc3d8ff5559411f82567b75f2e
Anna Tatinyan <anna.tatinyan.21@mail.ru>
2018-07-31 12:34:49 +0400

    React - front-end added

---
+++ client/src/components/login.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../index.css';
+
+
+class Login extends React.Component {
+
+
+  render() {
+      return (
+        <div class="board">
+        <h1>  Welcome to login page. I am not ready yet </h1>
+        </div>
+      );
+    }
+}
+
+
+export default Login;
